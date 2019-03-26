f3e198fbcbddcfaef3307fe98068976c75294f0b
Abhishek Shukla <47697131+abhishekshivshukla@users.noreply.github.com>
2019-03-26 15:13:29 +0530

    Update TestStudent.java

--- SeleniumPracticePrograms/src/office/TestStudent.java
+++ SeleniumPracticePrograms/src/office/TestStudent.java
@@ -8,6 +8,15 @@ public class TestStudent {
 		
 		Student s3 = new Student(103, "Alok Pandey", "alok@gmail.com");
 		Student s4 = new Student(104, "Shashank Shukla", "shashank@gmail.com");
+		
+		System.out.println(s2.getStudent_Id());
+		s2.setStudent_Address("Bangalore");
+		System.out.println(s2.getStudent_Address());
+		s1.setStudent_Address("Raebareli");
+		s3.setStudent_Address("Raebareli");
+		s4.setStudent_PhoneNumber(9455530580L);
+
+
 	
 	}
 }
