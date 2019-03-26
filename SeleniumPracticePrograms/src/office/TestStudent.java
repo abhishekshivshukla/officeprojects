387f2fb671f6249983f7e3f14753c8c8a0804c8a
abhishekshivshukla <abhishek.shiv.shukla@gmail.com>
2019-03-26 15:15:53 +0530

    commit 2

--- SeleniumPracticePrograms/src/office/TestStudent.java
+++ SeleniumPracticePrograms/src/office/TestStudent.java
@@ -4,8 +4,7 @@ public class TestStudent {
 
 	public static void main(String[] args) {
 		Student s1 = new Student(101, "Abhishek Shukla", 9532116688L);
-		Student s2 = new Student(102, "Ashutosh Pandey", 9415958468L);
-		
+		Student s2 = new Student(102, "Ashutosh Pandey", 9415958468L);		
 		Student s3 = new Student(103, "Alok Pandey", "alok@gmail.com");
 		Student s4 = new Student(104, "Shashank Shukla", "shashank@gmail.com");
 		
@@ -14,9 +13,6 @@ public class TestStudent {
 		System.out.println(s2.getStudent_Address());
 		s1.setStudent_Address("Raebareli");
 		s3.setStudent_Address("Raebareli");
-		s4.setStudent_PhoneNumber(9455530580L);
-
-
-	
+		s4.setStudent_PhoneNumber(9455530580L);	
 	}
 }
